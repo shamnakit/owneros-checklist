8f003afbef15876d02283632e6caa6a66a4747f6
shamnakit <shamankit@gmail.com>
2025-08-12 22:00:47 +0700

    group1r

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,2 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+// lib/supabase.ts
+export { supabase } from "@/utils/supabaseClient";
