358a49c932217f397edc8892a87d3e25febf6fe8
shamnakit <shamankit@gmail.com>
2025-09-04 15:33:44 +0700

    dev admin

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,6 @@
+import dynamic from "next/dynamic";
+const AdminDashboard = dynamic(() => import("@/components/admin/AdminDashboard"), { ssr: false });
+
+export default function AdminPage() {
+  return <AdminDashboard />;
+}
